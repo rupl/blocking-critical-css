07d10833bff8b0c7d639b5ea6358d8164d7ca2cb
Chris Ruppel <chris.ruppel@gmail.com>
2015-04-28 02:13:19 +0200

    :boom: initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Critical CSS that blocks rendering
+
+An example showing how inlined critical CSS can still block page renders.
+
+Creates an artificial delay before fetching an image requested within inline CSS.
---
+++ img.php
@@ -0,0 +1,10 @@
+<?php
+  // Allow for arbitrary delay via CSS url. Defaults to 5 seconds.
+  $delay = ($_GET['delay']) ? $_GET['delay'] : 5000;
+
+  // Artificial delay to simulate network latency (in milliseconds).
+  sleep($delay / 1000);
+
+  // Output the image.
+  header('Content-Type: image/jpeg');
+  readfile('jbay.jpg');
